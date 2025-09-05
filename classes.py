cbe6d38d41f36abcf17ef6cc8a1f366dea97061c
Yamacia <mathieumknguyen@gmail.com>
2025-09-05 14:45:19 +0200

    Side images are now correctly displayed when resizing the application

--- classes.py
+++ classes.py
@@ -25,9 +25,12 @@ class SideImage:
 
     def __init__(self, image, x, y, type):
         self.image = image
+        self.original_image = image
+        
         self.x = x
         self.y = y
 
+        self.type = type
         match type:
             case SideImagePosition.FAVOURITE:
                 self.text = "Your Favourite Image :"
@@ -59,14 +62,20 @@ class SideImage:
         Resizes the side image when resizing game screen
         """
         self.resizeImage(screen = screen)
+        self.resizeFont(screen = screen)
         self.reposition_x(screen = screen, position = position)
+        self.reposition_y(screen = screen)
 
     def resizeImage(self, screen):
         """
         Get the resized image
+
+        Keyword arguments:
+            screen: Surface
+                Surface resolution used for image representation
         """
         if screen.get_size() == (SCREEN_WIDTH, SCREEN_HEIGHT):
-            self.image = pygame.transform.scale(self.image, favouriteImageDimension.dimensions)
+            self.image = pygame.transform.scale(self.original_image, favouriteImageDimension.dimensions)
         
         else:
             # Get current screen dimensions
@@ -76,11 +85,57 @@ class SideImage:
             # Get button dimensions BASED on current screen dimensions
             image_width = screen_width * 0.25
             image_height = screen_height * 0.25
-            self.image = pygame.transform.scale(self.image, (image_width, image_height))
+            self.image = pygame.transform.scale(self.original_image, (image_width, image_height))
+
+    def resizeFont(self, screen):
+        """
+        Get the resized font
+
+        Keyword arguments:
+            screen: Surface
+                Surface resolution used for image representation
+        """
+        if screen.get_size() == (SCREEN_WIDTH, SCREEN_HEIGHT):
+            self.text_size = TINY_FONT_SIZE
+
+        else:
+            # Get current screen dimensions
+            screen_width = screen.get_width()
+            screen_height = screen.get_height()
+
+            # Get current screen ratios
+            screen_width_ratio = screen_width / SCREEN_WIDTH
+            screen_height_ratio = screen_height / SCREEN_HEIGHT
+
+            self.text_size = int(TINY_FONT_SIZE * min(screen_width_ratio, screen_height_ratio))
 
     def reposition_x(self, screen, position):
         """
         Repositions the side image when resizing game screen
+        on the x-axis
+
+        Keyword arguments:
+            screen: Surface
+                Surface resolution used for image representation
+            position: SideImagePosition(Enum)
+                the position of the image on the screen
         """
         if position == HomePosition.RIGHT:
             self.x = screen.get_width() - self.image.get_width()
+
+    def reposition_y(self, screen):
+        """
+        Repositions the side image when resizing game screen
+        on the y-axis
+
+        Keyword arguments:
+            screen: Surface
+                Surface resolution used for image representation
+        """
+        screen_height = screen.get_height()
+
+        match self.type:
+            case SideImagePosition.FAVOURITE:
+                self.y = - self.image.get_height()
+            case SideImagePosition.LAST:
+                self.y = screen_height / 2
